48904ca8cee2a394b2b20847e36985908b528e6a
Petter A. Urkedal <paurkedal@gmail.com>
2017-06-21 22:09:48 +0200

    Switch to jbuilder.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,13 +1,9 @@
 #! /usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
+#require "topkg-jbuilder"
+
 open Topkg
 
 let licenses = List.map Pkg.std_file ["COPYING.LESSER"; "COPYING"]
 
-let () = Pkg.describe ~licenses "mediawiki-api" @@ fun c ->
-  Ok [
-    Pkg.mllib "lib/mediawiki-api.mllib";
-    Pkg.bin ~dst:"mw-edit" "bin/mw_edit";
-    Pkg.bin ~dst:"mw-filter" "bin/mw_filter";
-  ]
+let () = Topkg_jbuilder.describe ~licenses ~name:"mediawiki-api" ()
